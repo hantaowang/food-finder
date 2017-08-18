6710ea223c82c89de0a318eb353381ccac75b20f
Will Wang <Hantao Wang>
2017-08-18 00:47:38 -0700

    completed auth and session id creation

--- backend/login/auth.py
+++ backend/login/auth.py
@@ -1,48 +1,30 @@
-from Crypto.Hash import SHA256
-import secrets, etcd, json
+import secrets, etcd, redis
+import hashlib, binascii
 
-e =  etcd.Client(host='0.0.0.0', port=2379)
+e = etcd.Client(host='0.0.0.0', port=2379)
 
-def generate_new_user(name):
-    if check_user_exists(name) == False:
+def generate_new_user(name, passw):
+    if check_user_exists(name):
         return "ERROR: User Already Exists"
-    salt1 = newsalt()
-    e.write(name + "/salt1", salt1)
-    return salt1   
-
-def get_user_salt(name):
-    if check_user_exists(name) == False:
-        return "ERROR: No Such User"
-    return e.read(name + "/salt1")
-
-def update_user_key(name, key1):
-    if check_user_exists(name) == False:
-        return "ERROR: No Such User"
-    hash = SHA256.new()
     salt = newsalt()
-    hash.update(salt + key1)
-    key2 = hash.digest()
-    e.write(name + "/key", key2)
-    e.write(name + "/salt2", salt2) 
+    key = hashlib.pbkdf2_hmac('sha256', passw.encode(), salt.encode(), 10000)
+    e.write(name + "/salt", salt)
+    e.write(name + "/key", binascii.hexlify(key))
+    return "Created new user " + name
 
 def newsalt(size=32):
     return secrets.token_hex(size)
 
 def check_user_exists(name):
     try:
-        e.read(name + "/salt1")
+        e.read(name + "/salt")
         return True
     except etcd.EtcdKeyNotFound:
         return False
 
-def validate(name, key):
+def validate(name, passw):
     if check_user_exists(name) == False:
         return "ERROR: No Such User"
-    hash = SHA256.new()
-    hash.update(e.read(name + "/salt2") + key1)
-    key2 = hash.digest()
-    return e.read(name + "/key") == key2
-
-
-
-
+    salt = e.read(name + "/salt").value
+    key = hashlib.pbkdf2_hmac('sha256', passw.encode(), salt.encode(), 10000)
+    return e.read(name + "/key").value == binascii.hexlify(key).decode()
