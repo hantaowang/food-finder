70a29d334afe6fa7c58edb8a88c0d7f15a84808b
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2017-08-26 15:13:46 -0700

    Fixed syntax error in urls.py and exemption error in views.py

--- backend/get_restaurants/views.py
+++ backend/get_restaurants/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from .yelpapi import *
 import redis
 
 r = redis.StrictRedis(host='0.0.0.0', port=6379)
 
+@csrf_exempt
 def index(request):
     try:
         session = request.POST['SESSION']
         location = request.POST['LOCATION']
         query_restaurants(session, location)
+        msg = "Success"
     except:
-        msg = "[ERROR] Missing SESSION_ID or LOCATION Parameter"
+        msg = "[ERROR] Missing SESSION or LOCATION Parameter"
     return HttpResponse(msg)
