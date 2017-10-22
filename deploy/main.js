f92f950c8bfd5d841d793fbc8b1bcb4135169e24
Hantao (WIll) Wang <hantaowang97@gmail.com>
2017-10-22 11:40:30 -0700

    fixed quilt spec for recent update

--- deploy/main.js
+++ deploy/main.js
@@ -1,32 +1,40 @@
-const {createDeployment, Machine, githubKeys, Container, Service, publicInternet} = require("@quilt/quilt");
+const quilt = require("@quilt/quilt");
 
-var namespace = createDeployment({namespace:"food-finder"});
+var namespace = quilt.createDeployment({namespace:"food-finder"});
 
-var base_machine = new Machine({
+var base_machine = new quilt.Machine({
     provider: "Amazon",
     size: 'm4.large',
-    sshKeys: githubKeys("hantaowang"), // Your github username here
+    sshKeys: quilt.githubKeys("hantaowang"), // Your github username here
     preemptible: true,
 });
 
 var Etcd = require("@quilt/etcd");
 var etcd = new Etcd.Etcd(2);
 
-var Redis = require("@quilt/redis");
+var Redis = require("@quilt/redis")
 var redis = new Redis(1, 'abcdef12345');
 
-var etcdhosts = ""
-etcd.cluster.forEach(function(c) {
-    etcdhosts += c.getHostname() + ",";
+function getHostname(c) {
+    return c.getHostname();
+}
+
+const etcdhosts = etcd.containers.map(getHostname).join(',');
+
+var app = new quilt.Container("django", "hantaowang/foodfinder", {
+  env: {
+    'redishost': redis.master.getHostname(),
+    'redispass': "abcdef12345",
+    'etcdhost': etcdhosts
+  }
 });
 
-var app = new Container("django", "hantaowang/foodfinder")
-    .withEnv({"redishost": redis.master.getHostname(), "etcdhost": etcdhosts, "redispass": "abcdef12345"});
+
 
 app.allowFrom(redis.master, 6379);
 redis.master.allowFrom(app, 6379);
 
-etcd.cluster.forEach(function(c) {
+etcd.containers.forEach(function(c) {
   app.allowFrom(c, 2379);
   c.allowFrom(app, 2379);
 });
